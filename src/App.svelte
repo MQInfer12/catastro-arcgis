8bc9754806f00348e8f9dd703d1395c0cc0aecbf
MQInfer12 <maummq@gmail.com>
2023-09-22 11:08:54 -0400

    Filtrado por distrito

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,10 @@
   import Map from '@arcgis/core/Map';
   import MapView from '@arcgis/core/views/MapView';
   import WMSLayer from "@arcgis/core/layers/WMSLayer.js";
+  import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer.js";
   import "@arcgis/core/assets/esri/themes/light/main.css";
+  import distritosData from './data/distritos.json';
+    import { onMount } from 'svelte';
 
   interface DataLayer {
     name: string,
@@ -45,7 +48,7 @@
     basemap: "gray-vector",
   });
 
-  const createMap = (domNode: string | HTMLDivElement | undefined) => {
+  const createMap = (domNode: string | HTMLDivElement | undefined) => {    
     const view = new MapView({
       container: domNode,
       map: map,
@@ -65,24 +68,82 @@
     } else {
       map.remove(l.layer);
     }
-  })
+  });
+
+  const url = `http://192.168.105.219:6080/arcgis/rest/services/planificacion/limites/MapServer/1/query?where=Nombre+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=geojson`
+  let data: any;
+  onMount(async () => {
+    data = await fetch(url).then(res => res.json());
+  });
+  let filter = "";
+  let geoJsonLayer: any;
+  let distrito: any;
+  const handleSearch = () => {
+    if(geoJsonLayer) {
+      map.remove(geoJsonLayer);
+    }
+    const filteredFeatures = data.features.filter((feature: any) => feature.properties.Nombre === filter)
+    if(filteredFeatures.length === 0) {
+      distrito = null;
+      alert("No hay resultados");
+      return;
+    }
+    distrito = filteredFeatures[0];
+    const showData = {
+      ...data,
+      features: filteredFeatures
+    }
+    const blob = new Blob([JSON.stringify(showData)], {
+      type: "application/json"
+    })
+    const url = URL.createObjectURL(blob);
+    geoJsonLayer = new GeoJSONLayer({
+      url: url
+    });
+    map.add(geoJsonLayer);
+  }
 </script>
 
 <main>
-  <div id="map" use:createMap></div>
-  {#if centerText}
-  <p>{centerText}</p>
-  {/if}
-  {#each dataLayers as layer, i}
-  <button on:click={() => handleToggleLayer(i)}>
-    Capa {layer.name}: {#if dataLayers[i].active} ON {:else} OFF {/if}
-  </button>
-  {/each}
+  <div>
+    <div id="map" use:createMap></div>
+    <div>
+      {#each dataLayers as layer, i}
+      <button on:click={() => handleToggleLayer(i)}>
+        Capa {layer.name}: {dataLayers[i].active ? "ON" : "OFF"}
+      </button>
+      {/each}
+    </div>
+  </div>
+  <div>
+    {#if centerText}
+    <p>{centerText}</p>
+    {/if}
+    <div>
+      <p>Buscar por distrito: </p>
+      <input type="text" bind:value={filter} />
+      <button on:click={handleSearch}>Buscar</button>
+      {#if distrito}
+      <div>
+        <p>FID: {distrito.properties.FID}</p>
+        <p>Nombre: {distrito.properties.Nombre}</p>
+        <p>Distrito: {distrito.properties.distrito}</p>
+        <p>Comuna: {distrito.properties.comuna}</p>
+        <p>Área: {distrito.properties.Shape_Area}</p>
+      </div>
+      {/if}
+    </div>
+  </div>
 </main>
 
 <style>
   #map {
     height: 500px;
     width: 500px;
+  }
+
+  main {
+    display: flex;
+    gap: 20px;
   }
 </style>
